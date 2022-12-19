a817658a7896afcfa754a57a3063b1352d215797
d-kimsuon <d-kimuson@gmail.com>
2022-12-20 03:02:45 +0900

    0.0.2

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-VERSION=0.0.1
+VERSION=0.0.2
 sed -i -e "s/\"[.0-9].[.0-9].[.0-9]\"/\"$VERSION\"/" ./package.json
-git add package.json
+git add package.json ./bin/deploy.sh
 git commit -m "$VERSION"
 git tag -a v$VERSION -m "$VERSION"
 git push origin HEAD
