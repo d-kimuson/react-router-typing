8781b41e5bca746f704f09171e9200beff6f0e03
d-kimsuon <d-kimuson@gmail.com>
2022-12-20 01:38:52 +0900

    feat(*): configure as publish

---
+++ src/index.ts
@@ -0,0 +1,5 @@
+export type { IRoutes, AsRouteConfig } from "./type/declare.type"
+
+export { generateUtils } from "./generate-utils"
+export { defineRoutes } from "./define-routes"
+export { asBrowserRouter } from "./as-browser-router"
